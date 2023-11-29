3dbc611eca3273006307655ea02e1187f14490ed
Ronaldo <Ronaldo-172010@hotmail.com>
2023-11-29 13:48:04 -0300

    atualizado versão gradle,firebase e packges

--- lib/main.dart
+++ lib/main.dart
@@ -2,21 +2,21 @@
 import 'dart:io';
 
 import 'package:firebase_core/firebase_core.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:whatsapp/GeraRotas.dart';
+import 'package:whatsapp/firebase_options.dart';
 import 'package:whatsapp/views/Login.dart';
-import 'package:whatsapp/views/inicio.dart';
 
 void main()  async{
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform
+  );
 
  final ThemeData ios =ThemeData(
        colorScheme: ColorScheme.fromSwatch().copyWith(onSecondary: Color(0xff25D366)),
        primaryColor:Colors.grey[200],
-      buttonTheme:ButtonThemeData(
+      buttonTheme:const ButtonThemeData(
           buttonColor: Colors.white
       ), 
   ) ;
@@ -24,7 +24,7 @@ void main()  async{
   final ThemeData temaPadrao =ThemeData(
       colorScheme: ColorScheme.fromSwatch().copyWith(secondary: Color(0xff25D366)),
       primaryColor: Color(0xff075E54),
-      buttonTheme:ButtonThemeData(
+      buttonTheme:const ButtonThemeData(
           buttonColor: Colors.white
       )
   ) ;
